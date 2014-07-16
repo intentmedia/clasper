dac5a3b1b9ef48265fde350d1e0632e535bb4e00
Chet Mancini <chetmancini@gmail.com>
2014-07-16 11:56:54 -0400

    CM: add project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject intentmedia/clasper "0.1.0"
+  :description "A ClojureScript API for CasperJS"
+  :url "https://github.com/intentmedia/clasper"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "Same as ClojureScript"})
